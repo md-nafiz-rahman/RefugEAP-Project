001db5deb7edeef807a496854bff845955738fa0
TKeegan <tk274@student.le.ac.uk>
2023-02-02 22:23:04 +0000

    Uploaded Project After Setting up Spring

---
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.example.refugeapproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping(value = "/") // Request to HomePage
+    public String HomePage() {
+        return "homePage";
+    }
+
+}
