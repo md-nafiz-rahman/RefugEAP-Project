38b0b7edac85d5efd270d3fedbb0cc424c2ce6ea
tk274 <tk274@student.le.ac.uk>
2023-02-03 12:43:06 +0000

    Added Refugee Spring Project (HC332)

---
+++ Project/RefugEAP-Project/src/test/java/com/example/refugeapproject/RefugEapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refugeapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugEapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
