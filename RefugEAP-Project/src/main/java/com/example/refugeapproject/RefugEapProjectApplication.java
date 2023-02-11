b9d3a8b4706540561d6ad9b052c690bf99a3b180
Trucktore <99349713+Trucktore@users.noreply.github.com>
2023-02-11 15:30:12 +0000

    fixed the project so that it works properly when importing via intellij

--- RefugEAP-Project/src/main/java/com/example/refugeapproject/RefugEapProjectApplication.java
+++ RefugEAP-Project/src/main/java/com/example/refugeapproject/RefugEapProjectApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RefugEapProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RefugEapProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RefugEapProjectApplication.class, args);
+    }
 
 }
