a59e336d78fc29583f4a82b8bd98686d733d5ad2
R Khoshnaw <rk407@student.le.ac.uk>
2023-02-11 15:35:53 +0000

    Revert "fixed the project so that it works properly when importing via intellij"
    This reverts commit b9d3a8b4706540561d6ad9b052c690bf99a3b180

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/RefugEapProjectApplication.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/RefugEapProjectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RefugEapProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RefugEapProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RefugEapProjectApplication.class, args);
+	}
+
 
 }
