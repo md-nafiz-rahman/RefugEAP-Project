480cf904582204e45551f7f7edda1db6bf4fd462
R Khoshnaw <rk407@student.le.ac.uk>
2023-02-11 21:39:06 +0000

    Revert "Merge branch 'test-branch' into 'main'"
    This reverts merge request !4

---
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/RefugEapProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.refugeapproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugEapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugEapProjectApplication.class, args);
+	}
+
+
+}
