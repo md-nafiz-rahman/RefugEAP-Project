10d8705c513120c0688adadd79558c41e20eb5f8
Harry Chambers <hc332@student.le.ac.uk>
2023-02-21 18:45:47 +0000

    Month class for calendar

---
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/Month.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Month {
+
+    private String name;
+
+    private int no_days;
+
+    List<String> events = Arrays.asList(new String[31]);
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNo_days() {
+        return no_days;
+    }
+
+    public void setNo_days(int no_days) {
+        this.no_days = no_days;
+    }
+
+    public List<String> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<String> events) {
+        this.events = events;
+    }
+}
