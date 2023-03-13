ac6e82899b6a5bbb84e578fcc73bc1387453fc54
Harry Chambers <hc332@student.le.ac.uk>
2023-03-13 17:44:07 +0000

    Query to get the number of entries form the blogs table

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/BlogRepo.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/BlogRepo.java
@@ -9,4 +9,8 @@ public interface BlogRepo extends CrudRepository<Blog,Integer> {
     @Query("SELECT b FROM Blog b WHERE b.blog_id = :blog_id")
     public Blog getBlogByBlog_id(@Param("blog_id") int blog_id);
 
+    @Query("SELECT count(b.blog_id) FROM Blog b") //Find the total number of entries in the blogs table
+    public long total_rows_in_blog ();
+
+
 }
