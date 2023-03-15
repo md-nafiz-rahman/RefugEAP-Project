e5254bfb92fead55a5c21d02858a64074a09dd0e
ys349 <ys349@student.le.ac.uk>
2023-03-15 08:03:48 +0000

    eventRepo updated

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/EventRepo.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/EventRepo.java
@@ -1,10 +1,14 @@
 package com.example.refugeapproject.membership;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventRepo extends CrudRepository<Event, Integer> {
     Event findById(int id);
     List<Event> findByStatus(String status);
+    @Query("SELECT e FROM Event e WHERE e.event_datetime BETWEEN :startDateTime AND :endDateTime")
+    List<Event> findByEventDatetimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
