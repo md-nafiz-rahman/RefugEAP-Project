93d3d1e91d4f85c93bc4139d855aaf650aecf99d
Harry Chambers <hc332@student.le.ac.uk>
2023-03-29 14:57:28 +0100

    Contact and contactRepo comments

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/ContactRepo.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/ContactRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/*  Interface for retrieving contacts via methods from the database  */
 public interface ContactRepo extends CrudRepository<Contact, Integer> {
     public Contact findById(int id);
     public List<Contact> findByStatus(String status);
