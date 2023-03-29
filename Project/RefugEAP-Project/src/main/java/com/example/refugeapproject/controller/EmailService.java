a9c4512a69a5bb024a7a362b5c585b954eb35209
Harry Chambers <hc332@student.le.ac.uk>
2023-03-29 14:16:55 +0100

    Email service comments

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/controller/EmailService.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/controller/EmailService.java
@@ -5,17 +5,20 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+
+/*  Class to send emails to users  */
 @Component
 public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
 
+    // This method creates a template email to quickly send
     public void sendMessage(
             String to, String subject, String text) {
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("Your email to send from");
+        message.setFrom("Your email to send from"); // Use the email you want to send emails from (to the client)
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
