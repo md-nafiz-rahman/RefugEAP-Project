dc7614298ea9bbfcc63d04cc2e4e5c971ebb6e92
Harry Chambers <hc332@student.le.ac.uk>
2023-03-29 14:55:03 +0100

    BlogRepo comments

--- Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/BlogRepo.java
+++ Project/RefugEAP-Project/src/main/java/com/example/refugeapproject/membership/BlogRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/*  Interface for retrieving blogs via methods from the database  */
 public interface BlogRepo extends CrudRepository<Blog, Integer> {
     public Blog findById(int id);
     public List<Blog> findByStatus(String status);
